f0304906965a4353a02362ab1d73a4e586260346
imjesusc <iamjesus.ds@gmail.com>
2024-11-13 01:05:44 +0800

    refactor: update styles in home-location component

--- src/components/organisms/home-location.tsx
+++ src/components/organisms/home-location.tsx
@@ -1,4 +1,3 @@
-import { lusitana } from '@/app/fonts/fonts'
 import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import imageUrl from '../../../public/assets/potencia-img-location.png'
@@ -6,34 +5,30 @@ import { Typography } from '../molecules'
 
 export const HomeLocation = () => {
 	return (
-		<div className="py-10 laptop:py-20 bg-black">
-			<div className="container space-y-10">
-				<Typography
-					as="h2"
-					className={cn(
-						'text-center font-bold bg-gradient-to-t from-[#B28AF2] to-[#3B1FA5] bg-clip-text text-transparent',
-						lusitana.className
-					)}
-					size="6xl"
-				>
-					Mapa del Evento
-				</Typography>
+		<section className="bg-black py-10 laptop:pt-36">
+			<div className="container space-y-10" id="mapa">
+				<header>
+					<Typography
+						as="h2"
+						className={cn(
+							'bg-gradient-to-t from-[#B28AF2] to-[#3B1FA5] bg-clip-text text-center font-bold text-transparent'
+						)}
+						size="6xl"
+					>
+						Mapa del Evento
+					</Typography>
+				</header>
 
-				<figure className="w-full h-auto rounded-none">
+				<figure className="h-auto w-full">
 					<Image
 						src={imageUrl}
 						alt="Imagen de ejemplo"
-						layout="responsive"
 						width={1600}
 						height={900}
-						sizes="(max-width: 768px) 100vw, 
-                                (max-width: 1024px) 50vw, 
-                                33vw"
-						className="w-full h-auto object-cover"
-						quality={100}
+						className="aspect-[261/139] h-auto w-full object-cover"
 					/>
 				</figure>
 			</div>
-		</div>
+		</section>
 	)
 }
