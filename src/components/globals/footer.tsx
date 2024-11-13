3cba33d766bf04d23aae6653b3f8e5eef1a19ebc
imjesusc <iamjesus.ds@gmail.com>
2024-11-14 00:20:32 +0800

    fix: update styles in footer component

--- src/components/globals/footer.tsx
+++ src/components/globals/footer.tsx
@@ -1,47 +1,38 @@
 import { Mail } from 'lucide-react'
-import Image from 'next/image'
 import Link from 'next/link'
-import logoUrl from '../../../public/assets/potencia-logo.svg'
 import { Separator } from '../ui/separator'
+import { PottenciaLogo } from './pottencia-logo'
 
 export const Footer = () => {
 	return (
 		<footer className="bg-black text-gray-300">
-			<div className="max-w-6xl mx-auto px-4 py-6">
-				<div className="flex flex-col md:flex-row justify-between items-center">
-					<div className="flex flex-col items-center md:items-start mb-4 md:mb-0">
-						<figure className="mb-2">
-							<Image src={logoUrl} alt="Potencia Logo" width={160} height={60} />
-						</figure>
-						<div className="flex items-center mb-2">
-							<Mail className="w-4 h-4 mr-2 text-purple-400" />
-							<Link href="mailto:potencia-logo.svg" className="hover:text-purple-400 transition-colors" target="_blank">
-								pottencia@lottus.com
-							</Link>
-						</div>
-					</div>
-				</div>
+			<div className="mx-auto flex max-w-6xl flex-col gap-y-10 px-4 py-10 laptop:px-0">
+				<section>
+					<figure>
+						<PottenciaLogo className="max-w-[150px]" />
+					</figure>
 
-				<Separator className="hidden md:block my-10 bg-gray-700" />
-
-				<div className="flex justify-between">
-					<div className="w-full md:w-auto flex">
-						<span className="text-sm whitespace-nowrap">© 2024 Scoit. All rights reserved.</span>
-					</div>
-
-					<p className="text-sm">
-						Developed with
-						<span className="mx-1 text-purple-400">&hearts;</span>
-						by
+					<footer className="mb-2 flex items-center">
+						<Mail className="mr-2 h-4 w-4 text-purple-400" />
 						<Link
-							href="https://www.linkedin.com/in/roycerdan"
+							href="mailto:potencia-logo.svg"
+							className="text-[#999] transition-colors hover:text-purple-400"
 							target="_blank"
-							className="ml-1 font-semibold hover:text-purple-400 transition-colors"
 						>
-							Roy
+							pottencia@lottus.com
 						</Link>
+					</footer>
+				</section>
+
+				<Separator className="bg-[#333]" />
+
+				<section className="flex flex-col justify-between gap-6 laptop:flex-row">
+					<span className="order-2 whitespace-nowrap text-sm text-[#999]">© 2024 Scoit. All rights reserved.</span>
+
+					<p className="text-sm text-[#999] laptop:order-2">
+						Built by <span className="text-white">Scoit Experience</span>
 					</p>
-				</div>
+				</section>
 			</div>
 		</footer>
 	)
