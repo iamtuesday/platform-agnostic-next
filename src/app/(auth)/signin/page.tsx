6665f553496dcdf47b6af4693aadcb4eb3cd4824
Roy Cerdan <roy.cerdan@somosari.com>
2024-11-14 15:25:04 -0500

    chore: update

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,14 +1,12 @@
-import Logo from '@/components/globals/logo'
+import { PottenciaLogo } from '@/components/globals'
 import { SignInForm } from '@/components/organisms'
 
 export default function LoginPage() {
 	return (
 		<main className="flex items-center justify-center md:h-screen">
 			<div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-				<div className="flex h-20 w-full items-end rounded-lg bg-primary p-3 md:h-36">
-					<div className="w-32 text-white md:w-36">
-						<Logo />
-					</div>
+				<div className="grid h-20 w-full place-content-center rounded-lg bg-primary p-3 md:h-36">
+					<PottenciaLogo />
 				</div>
 				<SignInForm />
 			</div>
--- src/components/globals/logo.tsx
+++ src/components/globals/logo.tsx
@@ -1,13 +1,9 @@
-import { lusitana } from '@/app/fonts/fonts';
-// import { Globe2Icon } from 'lucide-react';
+import { lusitana } from '@/app/fonts/fonts'
 
 export default function Logo() {
-  return (
-    <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
-    >
-      {/* <Globe2Icon className="h-12 w-12 rotate-[15deg]" /> */}
-      <p className="text-[44px] ">Agnostic</p>
-    </div>
-  );
+	return (
+		<div className={`${lusitana.className} flex flex-row items-center leading-none text-white`}>
+			<p className="text-[44px]">Agnostic</p>
+		</div>
+	)
 }
