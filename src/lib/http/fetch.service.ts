01cb7e6f0a26a72e6891a684bc1efc3933ea9748
imjesusc <iamjesus.ds@gmail.com>
2024-11-19 07:13:35 +0800

    feat: add fetch service

--- src/lib/http/fetch.service.ts
+++ src/lib/http/fetch.service.ts
@@ -1,37 +1,85 @@
-type FetchOptions = RequestInit & {
-	serverSide?: boolean
+type FetchResponse<T> = [T | null, Error | null]
+
+export interface FetchOptions {
+	method: 'GET' | 'POST' | 'PUT' | 'DELETE'
+	body?: BodyInit
+	headers?: HeadersInit
+	params?: URLSearchParams | Record<string, string>
 	token?: string
 }
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'localhost:3000'
 
-export const fetchService = async <T>(endpoint: string, options: FetchOptions = {}): Promise<T> => {
-	const { token, serverSide, headers, ...restOptions } = options
+const contentTypes: Record<string, <T>(response: Response) => Promise<T>> = {
+	'APPLICATION/JSON': <T>(response: Response) => response.json() as Promise<T>,
+	'TEXT/': <T>(response: Response) => response.text() as Promise<T>,
+	'APPLICATION/OCTET-STREAM': <T>(response: Response) => response.arrayBuffer() as Promise<T>,
+	'APPLICATION/PDF': <T>(response: Response) => response.blob() as Promise<T>
+}
 
-	const defaultHeaders = new Headers()
-	defaultHeaders.append('Accept', 'application/json')
-
-	if (token) {
-		defaultHeaders.set('Authorization', `Bearer ${token}`)
-	}
+export const fetchService = async <T>(url: string, options: FetchOptions): Promise<FetchResponse<T>> => {
+	const { method, body, headers = {}, params, token } = options
 
 	try {
-		const response = await fetch(`${API_BASE_URL}${endpoint}`, {
-			headers: defaultHeaders,
-			...restOptions,
-			next: {
-				revalidate: 3600
-			}
-		})
-
-		if (!response.ok) {
-			const errorData = await response.json()
-			throw new Error(`Error: ${errorData.message}`)
+		// Construcción de la URL con parámetros
+		let apiUrl = `${API_BASE_URL}${url}`
+		if (params) {
+			const queryParams = params instanceof URLSearchParams ? params.toString() : new URLSearchParams(params).toString()
+			apiUrl += `?${queryParams}`
 		}
 
-		return response.json() as Promise<T>
-	} catch (error) {
-		console.error(`Error en la solicitud HTTP: ${(error as any)?.message}`)
-		throw error
+		// Configuración de la solicitud
+
+		const defaultHeaders = new Headers()
+
+		if (token) {
+			defaultHeaders.set('Authorization', `Bearer ${token}`)
+		}
+
+		const requestOptions: RequestInit = {
+			method,
+			headers: { ...defaultHeaders, ...headers }
+		}
+
+		// Manejar el método POST y PUT
+		if (method === 'POST' || method === 'PUT') {
+			if (!body) {
+				throw new Error(`El cuerpo es obligatorio para las solicitudes con ${method}`)
+			}
+
+			requestOptions.body = body
+		}
+
+		// Eliminar cuerpo en GET o DELETE
+		if (method === 'DELETE' || method === 'GET') {
+			delete requestOptions.body
+		}
+
+		// Realizar la solicitud
+		const response = await fetch(apiUrl, requestOptions)
+
+		// Si la respuesta no es exitosa
+		if (!response.ok) {
+			throw new Error(`Error en la solicitud: ${response.statusText}`)
+		}
+
+		// Identificar el tipo de contenido
+		const contentType = (response.headers.get('Content-Type') || '').split(';')[0].trim()
+
+		// Manejo de la respuesta según el tipo de contenido
+		const handler = contentTypes[contentType.toUpperCase()]
+
+		let data: T | null = null
+
+		if (handler) {
+			data = await handler(response)
+		} else {
+			data = (await response.text()) as T
+		}
+
+		return [data, null]
+	} catch (err) {
+		console.error(err)
+		return [null, err as Error]
 	}
 }
