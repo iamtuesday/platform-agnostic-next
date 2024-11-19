d5a16182c7df8acef15afc180acf096c26ceaca0
imjesusc <iamjesus.ds@gmail.com>
2024-11-19 08:56:15 +0800

    feat: add middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,31 @@
-// import NextAuth from 'next-auth'
-// import { authConfig } from './auth.config'
+import { NextRequest, NextResponse } from 'next/server'
+import { getSession } from './lib/auth/session'
 
-// export default NextAuth(authConfig).auth
+const protectedRoutePatterns = [/^\/dashboard/, /^\/streaming/, /^\/red-social/]
+const authRoutes = ['/signin']
 
-// export const config = {
-// 	// https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-// 	matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)']
-// }
-export const middleware = () => {}
+export default async function middleware(req: NextRequest) {
+	// Validamos si la ruta actual es protegida
+	const path = req.nextUrl.pathname
+	const isProtectedRoute = protectedRoutePatterns.some(pattern => pattern.test(path))
+	const isAuthRoute = authRoutes.includes(path)
+
+	// Obtenemos la cookie de sesión
+	const session = await getSession()
+
+	// Si intenta acceder a una ruta protegida sin estar autenticado, redirecciona al login
+	if (isProtectedRoute && !session?.userId) {
+		return NextResponse.redirect(new URL('/signin', req.url))
+	}
+
+	// Si se intenta acceder a register o login estando autenticado, redirecciona a profile
+	if (isAuthRoute && session?.userId) {
+		return NextResponse.redirect(new URL('/streaming', req.url))
+	}
+
+	return NextResponse.next()
+}
+
+export const config = {
+	matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)']
+}
