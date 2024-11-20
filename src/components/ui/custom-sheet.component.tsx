368a300baaf4fd9efca37c9ffec22a1ddea9a2f6
imjesusc <iamjesus.ds@gmail.com>
2024-11-20 13:45:29 +0800

    feat: add custom-sheet

---
+++ src/components/ui/custom-sheet.component.tsx
@@ -0,0 +1,29 @@
+import { ReactNode, useState } from 'react'
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from './sheet'
+
+interface CustomSheetProps {
+	children: (handleOpen: (value: boolean) => void) => ReactNode
+	title: string
+	trigger: ReactNode
+}
+
+export const CustomSheet = ({ children, title, trigger }: CustomSheetProps) => {
+	const [open, setOpen] = useState(false)
+
+	const handleOpen = (value: boolean) => {
+		setOpen(value)
+	}
+
+	return (
+		<Sheet open={open} onOpenChange={setOpen}>
+			<SheetTrigger asChild>{trigger}</SheetTrigger>
+			<SheetContent>
+				<SheetHeader>
+					<SheetTitle>{title}</SheetTitle>
+				</SheetHeader>
+
+				<div className="py-10">{children(handleOpen)}</div>
+			</SheetContent>
+		</Sheet>
+	)
+}
