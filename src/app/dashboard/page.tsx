2977ed69c7ac7f9ab021133edf12c95e1fb8909a
imjesusc <iamjesus.ds@gmail.com>
2024-11-20 09:58:35 +0800

    feat: add data-table component and add users page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,39 +1,40 @@
-export default async function DashboardPage() {
-	return <></>
-	// const token = getSession()
-	// const { getUsers } = await DashboardModule(`${token}`)
-	// const { list } = await CountryModule()
-	// const [users, error] = await getUsers()
-	// const [countries, _error] = await list()
+import { getUsers } from '@/actions/dashboard/actions'
+import { usersTableColumns } from '@/components/molecules'
+import { DataTable } from '@/components/organisms'
+import { mapUserOutputToUserTable } from '@/mappers'
 
-	// if (!!error || !!_error) {
-	// 	return {
-	// 		notFound: true
-	// 	}
-	// }
+export default async function DashboardPage({
+	searchParams
+}: {
+	searchParams?: {
+		search?: string
+		page?: string
+		size?: string
+	}
+}) {
+	const params = {
+		pageSize: Number(searchParams?.size) || 10,
+		pageNumber: Number(searchParams?.page) || 1,
+		term: searchParams?.search || ''
+	}
 
-	// /**
-	//  * TODO: La lista de usuarios debe estar en el modulo de usuarios, no hay necesidad de crear un module para el dashboard, a menos que sea para la entidades y ui
-	//  */
-	// const dataUsers = users?.map(user => {
-	// 	return {
-	// 		id: user.id,
-	// 		email: user.email,
-	// 		firstName: user.firstName,
-	// 		lastName: user.paternalLastName + ' ' + user.maternalLastName,
-	// 		countryId: countries?.find(country => country.value === user.countryId)?.name || '',
-	// 		phone: user?.phone?.prefix && user?.phone?.number ? `${user.phone.prefix} ${user.phone.number}` : 'Sin teléfono',
-	// 		profile: user.profile
-	// 	}
-	// })
+	console.log(params)
 
-	// return (
-	// 	<DataTable
-	// 		filterBy="email"
-	// 		filterPlaceholder="Filtrar por email"
-	// 		columns={usersTableColumns}
-	// 		data={dataUsers!}
-	// 		classNames={{ card: 'grid grid-cols-[auto_1fr] space-y-0 gap-y-1 gap-x-2' }}
-	// 	/>
-	// )
+	const users = await getUsers(params)
+
+	const mappedUsers = users?.items?.map(mapUserOutputToUserTable)
+
+	return (
+		<div className="min-h-[70vh]">
+			<DataTable
+				isServerSide
+				totalItems={users?.totalItems || 0}
+				filterBy="email"
+				filterPlaceholder="Filtrar por email"
+				columns={usersTableColumns}
+				data={mappedUsers || []}
+				classNames={{ card: 'grid grid-cols-[auto_1fr] space-y-0 gap-y-1 gap-x-2' }}
+			/>
+		</div>
+	)
 }
---
+++ src/mappers/user-output-to-user-table.map.ts
@@ -0,0 +1,13 @@
+import { IUserInfo, IUserInfoTable } from '@/interfaces'
+
+export const mapUserOutputToUserTable = (userInfo: IUserInfo): IUserInfoTable => {
+	return {
+		id: userInfo.id,
+		fullName: userInfo.fullName || 'desconocido',
+		email: userInfo.email || 'desconocido',
+		campus: userInfo.campus || 'desconocido',
+		firstLogin: userInfo.firstLogin,
+		mode: userInfo.mode || 'desconocido',
+		country: userInfo.country || 'desconocido'
+	}
+}
