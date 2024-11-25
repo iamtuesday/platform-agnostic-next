94b02468929cbf77508c893fc2a39574759bbec1
imjesusc <iamjesus.ds@gmail.com>
2024-11-26 00:43:51 +0800

    fix: signin page

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -4,7 +4,7 @@ import { SignInForm } from '@/components/organisms'
 export default function LoginPage() {
 	return (
 		<main className="flex items-center justify-center md:h-screen">
-			<div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+			<div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4">
 				<div className="grid h-20 w-full place-content-center rounded-lg bg-primary p-3 md:h-36">
 					<PottenciaLogo />
 				</div>
