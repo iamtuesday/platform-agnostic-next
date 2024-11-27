af3a843b56300f2bd523e6670ca1c33fb4a247a6
imjesusc <iamjesus.ds@gmail.com>
2024-11-27 11:05:01 +0800

    chore: fix signin and reel-card

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -5,12 +5,12 @@ import { RevokeSessionsDialog } from '@/components/organisms/revoked-session-dia
 export default function LoginPage() {
 	return (
 		<main className="flex items-center justify-center md:h-screen">
-			<div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4">
+			<div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-6 p-4">
 				<header className="grid h-20 w-full place-content-center rounded-lg bg-primary p-3 md:h-36">
 					<PottenciaLogo />
 				</header>
 
-				<section className="mt-6 flex h-full w-full flex-col items-center gap-4">
+				<section className="flex h-full w-full flex-col items-center gap-4">
 					<SignInForm />
 					<RevokeSessionsDialog />
 				</section>
--- src/components/molecules/reel-card.component.tsx
+++ src/components/molecules/reel-card.component.tsx
@@ -7,14 +7,16 @@ interface ReelCardProps {
 }
 
 export const ReelCard = ({ reel }: ReelCardProps) => {
-	const iframeUrl =
-		reel?.url ||
-		`<iframe allow="fullscreen" allowfullscreen src="https://streamable.com/e/61cwm7?" style="border:none; width:100%; height:100%; position:absolute; left:0px; top:0px; overflow:hidden;"></iframe>`
+	const iframeUrlFormat = reel?.url ? reel.url : '<iframe></iframe>'
+
+	const sanitizedIframe = iframeUrlFormat
+		.replace(/style="[^"]*"/gi, '')
+		.replace(/<iframe/gi, `<iframe className="aspect-[9/16] bg-transparent h-full w-full"`)
 
 	return (
 		<article className="w-full space-y-2">
 			<div className="relative h-[450px] w-full overflow-hidden rounded-lg border bg-gray-100">
-				<Mdx>{iframeUrl.replace(/<iframe/gi, `<iframe className="absolute top-0 left-0 h-full w-full"`)}</Mdx>
+				<Mdx className="h-full w-full bg-black">{sanitizedIframe}</Mdx>
 			</div>
 
 			<Typography as="h3" weight="semibold">
